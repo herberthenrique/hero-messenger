b41d7c7fe6219f5f8bc4e91f39e96675c2e88920
Herbert Henrique <eu@herberthenrique.com>
2016-10-30 16:11:02 -0200

    Criando evento para enviar mensagens

--- src/js/controller/message.controller.js
+++ src/js/controller/message.controller.js
@@ -1,8 +1,7 @@
 /* global document */
-// eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved
-import templateMessage from 'pug!../../templates/message.pug';
-// eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved
-import templateContainer from 'pug!../../templates/messenger.pug';
+
+import templateMessage from 'pug!../../templates/message.pug'; // eslint-disable-line import/no-extraneous-dependencies, import/no-unresolved
+import templateContainer from 'pug!../../templates/messenger.pug'; // eslint-disable-line import/no-extraneous-dependencies, import/no-unresolved
 import MessageService from '../service/message.service';
 import * as messageHelper from '../helper/message.helper';
 import * as helper from '../helper/general.helper';
@@ -18,11 +17,22 @@ export default class MessageController {
     const activeMessage = this.service.getConversation(this.id);
     helper.appendHtmlWithTemplate(templateContainer, 'app', activeMessage);
     this.addList(activeMessage.messages);
+    document.getElementById('form-send-message').addEventListener('submit', (e) => {
+      e.preventDefault();
+      const messageValue = document.getElementById('message-content').value;
+      this.send(messageValue);
+      return false;
+    });
   }
 
-  // send(message) {
-
-  // }
+  send(message) {
+    if (message) {
+      const result = this.service.send(message, 1);
+      const formattedMessage = messageHelper.formatMessage(result);
+      helper.appendHtmlWithTemplate(templateMessage, 'messages-container', formattedMessage);
+      document.getElementById('message-content').value = '';
+    }
+  }
 
   // eslint-disable-next-line class-methods-use-this
   addList(messages) {
